486ea56193b963563cc4ea2aecb20691e3817df9
PabloDeGroot <105039324+PabloDeGroot@users.noreply.github.com>
2024-12-05 17:00:03 +0100

    Add mute toggle functionality to Dream component; update muted state based on user activation

--- hosting/src/lib/Components/Dream.svelte
+++ hosting/src/lib/Components/Dream.svelte
@@ -8,13 +8,14 @@
         interarctive: boolean;
     }
     let { stream }: DreamProps = $props();
-    let muted = $state(true);
+    let muted = $state(!navigator.userActivation.isActive);
     
     let videoSource: HTMLVideoElement;
     
     $effect(() => {
         videoSource.muted = muted;
         videoSource.srcObject = stream;
+        
         try{
             videoSource.play();
         }
@@ -22,9 +23,13 @@
             console.log(e);
         }
     });
+    let toggleMute = () => {
+        muted = !muted;
+    };
 </script>
 
 <div>
     <!-- svelte-ignore a11y_media_has_caption -->
     <video  bind:this={videoSource} autoplay></video>
+    <button onclick={toggleMute}>{muted ? "Unmute" : "Mute"}</button>
 </div>
